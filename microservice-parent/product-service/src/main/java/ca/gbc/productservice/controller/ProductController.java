3527482b424f48d16830593d4a8604d5f9e7f405
WillingtonSt <145478368+WillingtonSt@users.noreply.github.com>
2024-09-28 15:34:22 -0400

    Updated missing code

--- microservice-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
+++ microservice-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,12 +19,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+    private final ProductServiceImpl productService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest){
-        productService.createProduct(productRequest);
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest){
+
+        ProductResponse createdProduct = productService.createProduct(productRequest);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/product/" + createdProduct.id());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(createdProduct);
+
     }
+
+
+
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -33,20 +49,21 @@ public class ProductController {
 
 
     //http://localhost:8082/api/product/
-    @PutMapping
+    @PutMapping("/{productId}")
    // @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<?> updateProduct(@PathVariable("productId") String productId,
                                            @RequestBody ProductRequest productRequest){
 
        String updatedProductId = productService.updateProduct(productId, productRequest);
 
-       //set the location heafer attribute
+       //set the location header attribute
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/product" + updatedProductId);
 
         return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
     }
 
+    @DeleteMapping("/{productId}")
     public ResponseEntity<?> deleteProduct(@PathVariable("productId") String productId) {
         productService.deleteProduct(productId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- microservice-parent/product-service/src/main/java/ca/gbc/productservice/dto/ProductResponse.java
+++ microservice-parent/product-service/src/main/java/ca/gbc/productservice/dto/ProductResponse.java
@@ -8,3 +8,6 @@ public record ProductResponse(
         String description,
         BigDecimal price
 ) {}
+
+
+
